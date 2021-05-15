973e69f8036432676c0be68daf373bca33e531bf
Swanand01 <75439077+Swanand01@users.noreply.github.com>
2021-05-15 22:29:32 +0530

    Added comparison plots on player dashboard

--- app.py
+++ app.py
@@ -24,117 +24,118 @@ def head_to_head(team_1, team_2):
 page = st.sidebar.radio('Navigation', ['Home', 'Player Dashboard'])
 
 if page == 'Home':
-
-    fig = go.Figure(data=[go.Bar(
-        name = 'Home win %',
-        x = team_stats['team'],
-        y = team_stats['home_win_percentage']
-    ),
-                        go.Bar(
-        name = 'Away win %',
-        x = team_stats['team'],
-        y = team_stats['away_win_percentage'],
-        marker_color = 'rgb(128, 0, 128)'
-    )
-    ])
-    fig.update_xaxes(tickangle=300)
-    fig.update_layout({
-    'title_text': "Home win % and away win %",
-    'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-    'paper_bgcolor': 'rgba(0, 0, 0, 0)',
-    })
-    st.plotly_chart(fig)
-
-
-    fig = px.bar(x=player_data['player'][:15], y=player_data['total_runs'][:15], title='Top run scorers', labels={'x': '', 'y':'Runs scored'})
-    fig.update_xaxes(tickangle=300)
-    fig.update_layout({
-    'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-    'paper_bgcolor': 'rgba(0, 0, 0, 0)',
-    })
-    st.plotly_chart(fig)
-
-
-    fig = px.bar(x=player_data['player'][:15], y=player_data['average'][:15], title='Averages of top run scorers', labels={'x': '', 'y':'Average'})
-    fig.update_xaxes(tickangle=300)
-    fig.update_layout({
-    'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-    'paper_bgcolor': 'rgba(0, 0, 0, 0)',
-    })
-    st.plotly_chart(fig)
-
-
-    fig = px.bar(x=player_data['player'][:15], y=player_data['strikerate'][:15], title='Strike rates of top run scorers', labels={'x': '', 'y':'Strike rate'})
-    fig.update_xaxes(tickangle=300)
-    fig.update_layout({
-    'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-    'paper_bgcolor': 'rgba(0, 0, 0, 0)',
-    })
-    st.plotly_chart(fig)
-
-
-    inn_1_score_venue = []
-    inn_2_score_venue = []
-    for venue in matches['venue'].unique():
-        inn_1_score_venue.append(round(matches[matches['venue'] == venue]['inning_1_score'].mean()))
-        inn_2_score_venue.append(round(matches[matches['venue'] == venue]['inning_2_score'].mean()))
-
-    fig = go.Figure(data=[go.Bar(
-        name = 'Avg. 1st inning score',
-        x = inn_1_score_venue,
-        y = list(matches['venue'].unique()),
-        orientation='h' 
-    ),
-                        go.Bar(
-        name = 'Avg. 2nd inning score',
-        x = inn_2_score_venue,
-        y = list(matches['venue'].unique()),
-        marker_color = 'rgb(128, 0, 128)', 
-        orientation='h'
-    )
-    ])
-    fig.update_layout({
-        'title_text': "Avg. first and second inning scores (view fullscreen)",
-        'height': 1000,
-        'width': 900,
+    column_1, column_2 = st.beta_columns(2)
+    with column_1:
+        fig = go.Figure(data=[go.Bar(
+            name = 'Home win %',
+            x = team_stats['team'],
+            y = team_stats['home_win_percentage']
+        ),
+                            go.Bar(
+            name = 'Away win %',
+            x = team_stats['team'],
+            y = team_stats['away_win_percentage'],
+            marker_color = 'rgb(128, 0, 128)'
+        )
+        ])
+        fig.update_xaxes(tickangle=300)
+        fig.update_layout({
+        'title_text': "Home win % and away win %",
         'plot_bgcolor': 'rgba(0, 0, 0, 0)',
         'paper_bgcolor': 'rgba(0, 0, 0, 0)',
-    })
-    st.plotly_chart(fig, use_container_width=False)
+        })
+        st.plotly_chart(fig)
 
 
-    subfig = make_subplots(rows=1, cols=2, subplot_titles=("Most matches won batting first", "Most matches won chasing"))
-    subfig.add_bar(row=1, col=1, y=list(dict(matches[matches['win_by_runs'] > 0]['venue'].value_counts()[:5]).values()), x=list(dict(matches[matches['win_by_runs'] > 0]['venue'].value_counts()[:5]).keys()))
-    subfig.add_bar(marker_color='rgb(128,0,128)',row=1, col=2, x=list(dict(matches[matches['win_by_wickets'] > 0]['venue'].value_counts()[:5]).keys()), y=list(dict(matches[matches['win_by_wickets'] > 0]['venue'].value_counts()[:5]).values()))
-    subfig.update_xaxes(tickangle=320)
-    subfig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-        'paper_bgcolor': 'rgba(0, 0, 0, 0)'} ,showlegend=False)
-    st.plotly_chart(subfig)
-
-
-    fig = px.bar( x=wickets[wickets['dismissal_kind'] != 'run out']['bowler'].value_counts()[:10][::-1], y=wickets[wickets['dismissal_kind'] != 'run out']['bowler'].value_counts().index[:10][::-1], orientation='h', title='Leading wicket takers', labels={'x':'Wickets', 'y':''})
-    fig.update_layout({
-        'height': 600,
+        fig = px.bar(x=player_data['player'][:15], y=player_data['total_runs'][:15], title='Top run scorers', labels={'x': '', 'y':'Runs scored'})
+        fig.update_xaxes(tickangle=300)
+        fig.update_layout({
         'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-        'paper_bgcolor': 'rgba(0, 0, 0, 0)'
-    })
-    st.plotly_chart(fig)
+        'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+        })
+        st.plotly_chart(fig)
 
 
-    st.text('Most successful pacers: SL Malinga, DJ Bravo')
-    st.text('Most successful spinners: P Chawla, S Narine')
+        fig = px.bar(x=player_data['player'][:15], y=player_data['average'][:15], title='Averages of top run scorers', labels={'x': '', 'y':'Average'})
+        fig.update_xaxes(tickangle=300)
+        fig.update_layout({
+        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+        'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+        })
+        st.plotly_chart(fig)
 
-    fig = px.bar(data_frame=pd.DataFrame(matches['player_of_match'].value_counts()[:10]), title='Most no. of MOTM awards won', labels={'index': '', 'value':'No. of awards'})
-    fig.update_xaxes(tickangle=330)
-    fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-        'paper_bgcolor': 'rgba(0, 0, 0, 0)'}, showlegend=False)  
-    st.plotly_chart(fig)
 
-    st.subheader('Head to Head')
-    team_1 = st.selectbox('Choose first team', team_tuple)
-    team_2 = st.selectbox('Choose second team', team_tuple)
-    if st.button('Go'):
-        head_to_head(team_1, team_2)
+        fig = px.bar(x=player_data['player'][:15], y=player_data['strikerate'][:15], title='Strike rates of top run scorers', labels={'x': '', 'y':'Strike rate'})
+        fig.update_xaxes(tickangle=300)
+        fig.update_layout({
+        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+        'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+        })
+        st.plotly_chart(fig)
+
+
+        inn_1_score_venue = []
+        inn_2_score_venue = []
+        for venue in matches['venue'].unique():
+            inn_1_score_venue.append(round(matches[matches['venue'] == venue]['inning_1_score'].mean()))
+            inn_2_score_venue.append(round(matches[matches['venue'] == venue]['inning_2_score'].mean()))
+
+        fig = go.Figure(data=[go.Bar(
+            name = 'Avg. 1st inning score',
+            x = inn_1_score_venue,
+            y = list(matches['venue'].unique()),
+            orientation='h' 
+        ),
+                            go.Bar(
+            name = 'Avg. 2nd inning score',
+            x = inn_2_score_venue,
+            y = list(matches['venue'].unique()),
+            marker_color = 'rgb(128, 0, 128)', 
+            orientation='h'
+        )
+        ])
+        fig.update_layout({
+            'title_text': "Avg. first and second inning scores (view fullscreen)",
+            'height': 1000,
+            'width': 900,
+            'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+            'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+        })
+        st.plotly_chart(fig, use_container_width=False)
+
+
+        subfig = make_subplots(rows=1, cols=2, subplot_titles=("Most matches won batting first", "Most matches won chasing"))
+        subfig.add_bar(row=1, col=1, y=list(dict(matches[matches['win_by_runs'] > 0]['venue'].value_counts()[:5]).values()), x=list(dict(matches[matches['win_by_runs'] > 0]['venue'].value_counts()[:5]).keys()))
+        subfig.add_bar(marker_color='rgb(128,0,128)',row=1, col=2, x=list(dict(matches[matches['win_by_wickets'] > 0]['venue'].value_counts()[:5]).keys()), y=list(dict(matches[matches['win_by_wickets'] > 0]['venue'].value_counts()[:5]).values()))
+        subfig.update_xaxes(tickangle=320)
+        subfig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+            'paper_bgcolor': 'rgba(0, 0, 0, 0)'} ,showlegend=False)
+        st.plotly_chart(subfig)
+
+
+        fig = px.bar( x=wickets[wickets['dismissal_kind'] != 'run out']['bowler'].value_counts()[:10][::-1], y=wickets[wickets['dismissal_kind'] != 'run out']['bowler'].value_counts().index[:10][::-1], orientation='h', title='Leading wicket takers', labels={'x':'Wickets', 'y':''})
+        fig.update_layout({
+            'height': 600,
+            'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+            'paper_bgcolor': 'rgba(0, 0, 0, 0)'
+        })
+        st.plotly_chart(fig)
+
+
+        st.text('Most successful pacers: SL Malinga, DJ Bravo')
+        st.text('Most successful spinners: P Chawla, S Narine')
+
+        fig = px.bar(data_frame=pd.DataFrame(matches['player_of_match'].value_counts()[:10]), title='Most no. of MOTM awards won', labels={'index': '', 'value':'No. of awards'})
+        fig.update_xaxes(tickangle=330)
+        fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+            'paper_bgcolor': 'rgba(0, 0, 0, 0)'}, showlegend=False)  
+        st.plotly_chart(fig)
+
+        st.subheader('Head to Head')
+        team_1 = st.selectbox('Choose first team', team_tuple)
+        team_2 = st.selectbox('Choose second team', team_tuple)
+        if st.button('Go'):
+            head_to_head(team_1, team_2)
     
 
 elif page == 'Player Dashboard':
@@ -158,16 +159,16 @@ elif page == 'Player Dashboard':
         if season == 'All':
             st.write('Average: ', round(record['average'].values[0]))
         else:
-            st.write('Average: ', season_avg)
-
+            st.write('Average: ', round(season_avg))
+   
     with col1:
         st.write('Strike rate: ', round(record['strikerate'].values[0]))
-
     with col2:
         if season == 'All':
             st.write('Wickets taken: ', record['wickets'].values[0])
         else:
             st.write('Wickets taken: ', len(season_wicket_record))
+    
     with col1:
         if season == 'All':
             st.write('Catches taken: ', len(catch_record))
@@ -178,3 +179,24 @@ elif page == 'Player Dashboard':
             st.write('Run outs: ', len(run_out_record))
         else:
             st.write('Run outs: ', len(season_run_out_record))
+
+   
+    fig = px.scatter(x=player_data['player'], y=player_data['average'], labels={'x':'', 'y':'Average'})
+    fig.add_scatter(x=player_data[player_data['player'] == player]['player'], y=player_data[player_data['player'] == player]['average'], marker=dict(color="crimson", size=8),)
+    fig.update_xaxes(showticklabels=False, showgrid=False, zeroline=False)
+    fig.update_yaxes(showgrid=False, zeroline=False)
+    fig.update_layout({
+    'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+    'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+    },showlegend=False)
+    st.plotly_chart(fig)
+
+    fig = px.scatter(x=player_data['player'], y=player_data['strikerate'], labels={'x':'', 'y':'Strike rate'})
+    fig.add_scatter(x=player_data[player_data['player'] == player]['player'], y=player_data[player_data['player'] == player]['strikerate'], marker=dict(color="crimson", size=8),)
+    fig.update_xaxes(showticklabels=False, showgrid=False, zeroline=False)
+    fig.update_yaxes(showgrid=False, zeroline=False)
+    fig.update_layout({
+    'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+    'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+    },showlegend=False)
+    st.plotly_chart(fig)
